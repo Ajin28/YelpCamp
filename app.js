9cedf7e7524108be0663f72c4a4a7cdea54b22d7
Ajin28 <amisha28jain@gmail.com>
2020-09-13 07:03:47 +0530

    Added Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
+
+// var campgrounds = [
+//     { name: "Lake Mantade", image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+//     { name: "Mounatain Creek", image: "https://images.unsplash.com/photo-1581205445756-15c1d2e9a8df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+//     { name: "Kaheti Forest", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+//     { name: "Lake Mantade", image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+//     { name: "Mounatain Creek", image: "https://images.unsplash.com/photo-1581205445756-15c1d2e9a8df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
+//     { name: "Kaheti Forest", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" }
+// ]
+
+//Connecting DB
+mongoose.connect('mongodb://localhost:27017/YelpCamp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
+
+// Schema setup
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+//Defining a model
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+//Adding campgrounds
+// Campground.create(campgrounds, function (err, res) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log(res);
+//     }
+// })
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -9,25 +45,33 @@ app.get("/", (req, res) => {
     res.render("landing");
 });
 
-var campgrounds = [
-    { name: "Lake Mantade", image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
-    { name: "Mounatain Creek", image: "https://images.unsplash.com/photo-1581205445756-15c1d2e9a8df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
-    { name: "Kaheti Forest", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
-    { name: "Lake Mantade", image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
-    { name: "Mounatain Creek", image: "https://images.unsplash.com/photo-1581205445756-15c1d2e9a8df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" },
-    { name: "Kaheti Forest", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" }
-]
-
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds });
+    //Retrieving Campgrounds from database
+    Campground.find({}, function (err, campgrounds) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("campgrounds", { campgrounds });
+        }
+    })
+
 });
 
 app.post("/campgrounds", (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
     let campground = { name, image };
-    campgrounds.push(campground);
-    res.redirect("/campgrounds")
+    //  Adding new campground to database
+    Campground.create(campground, function (err, newCampground) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect("/campgrounds")
+        }
+    })
+
 });
 
 app.get("/campgrounds/new", (req, res) => {
