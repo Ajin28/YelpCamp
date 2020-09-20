bea09ec5dd67d3bc8765205ea30731c34d4165d5
Ajin28 <amisha28jain@gmail.com>
2020-09-20 19:20:36 +0530

    Update and Edit Comment  routes

--- routes/comments.js
+++ routes/comments.js
@@ -44,6 +44,34 @@ router.post("/", function (req, res) {
     })
 })
 
+// EDIT comment
+router.get("/:comment_id/edit", function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment })
+        }
+    })
+})
+
+// UPDATE comment
+router.put("/:comment_id", function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) {
+            console.log(err);
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    })
+})
+
+// DELETE comment
+
+
+
 //middleware - to add a comment user must be looged in
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
