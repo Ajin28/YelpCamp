00f457a8254e1b425287e9513f097c00363e10a6
Ajin28 <amisha28jain@gmail.com>
2020-09-21 19:40:46 +0530

    Seed.js updated

--- seed.js
+++ seed.js
@@ -38,35 +38,39 @@ var users = [
 
 var seeds = [
     {
-
+        price: Math.round(Math.random() * 10),
         name: "Lake Mantade",
         image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         description: "A beautiful lake surrounded by trees and native flora.Good place for bird watching and camping enthusiasts",
 
     },
     {
-
+        price: Math.round(Math.random() * 10),
         name: "Mounatain Creek",
         image: "https://images.unsplash.com/photo-1581205445756-15c1d2e9a8df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         description: "Breathtaking mountain views. Great for Camping.",
 
     },
     {
+        price: Math.round(Math.random() * 10),
         name: "Kaheti Forest",
         image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         description: "Get back to nature in this mesmerizing tropical forest. Witness wildlife in its inate form and find your inate instincts"
     },
     {
+        price: Math.round(Math.random() * 10),
         name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupiseedst non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
+        price: Math.round(Math.random() * 10),
         name: "Desert Mesa",
         image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupiseedst non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
+        price: Math.round(Math.random() * 10),
         name: "Canyon Floor",
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupiseedst non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
